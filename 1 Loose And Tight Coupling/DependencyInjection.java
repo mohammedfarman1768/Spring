8ff2bc7cb08c455491c7b8f8302ad88856b0fd4d
Farman <farmankhan1768@gmail.com>
2026-02-26 00:18:32 +0530

    third

--- 1 Loose And Tight Coupling/DependencyInjection.java
+++ 1 Loose And Tight Coupling/DependencyInjection.java
@@ -5,7 +5,7 @@ interface NotificationService {
 
 // ---------- IMPLEMENTATIONS ----------
 class EmailService implements NotificationService {
-    public void send(String message) {
+    public void send(String message) { // 
         System.out.println("Email sent: " + message);
     }
 }
